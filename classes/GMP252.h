13b036319a21b900e8a8ea4a81e9f8eaacdf0607
darkkii <anton_saarinen@hotmail.com>
2024-02-29 19:45:39 +0200

    added tentative private variables to hold sensor data

--- classes/GMP252.h
+++ classes/GMP252.h
@@ -1,15 +1,18 @@
 #ifndef GMP252_H
 #define GMP252_H
 
-// #include "ModbusClient.h"
+#include <memory>
+#include "ModbusClient.h"
 
 class GMP252
 {
 private:
-    float mCO2Measurement;
-    float mTemperatureCompensation;
-    float mTemperatureMeasurement;
-    // uint
+    float mCO2MeasurementPPM;
+    float mTemperatureCompensationC;
+    float mTemperatureMeasurementC;
+    int16_t mCO2MeasurementPPMLimited;
+    int16_t mCO2MeasurementPPMScaled;
+    std::shared_ptr<ModbusClient> mModbus;
 
 public:
     GMP252(/* args */);
