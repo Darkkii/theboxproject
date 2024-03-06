0d00672ae1c60641a459441da9ffb57c53a93f89
darkkii <anton_saarinen@hotmail.com>
2024-03-06 18:28:48 +0200

    prototype read member functions to handle reading from eeprom

--- classes/Eeprom.cpp
+++ classes/Eeprom.cpp
@@ -35,10 +35,17 @@ void Eeprom::write(const uint16_t readWriteAddress, const uint8_t *writeBuffer, 
 }
 
 void Eeprom::read(uint8_t *readBuffer, size_t bytesToRead)
-{}
+{
+    mWaitUntilReady();
+    i2c_write_blocking(mI2CHandler->getI2CBus(mI2CBusNumber), mI2CDeviceAddress, mCurrentReadWriteAddress, 2, true);
+    i2c_read_blocking(mI2CHandler->getI2CBus(mI2CBusNumber), mI2CDeviceAddress, readBuffer, bytesToRead, false);
+}
 
 void Eeprom::read(const uint16_t readWriteAddress, uint8_t *readBuffer, size_t bytesToRead)
-{}
+{
+    mSetReadWriteAddress(readWriteAddress);
+    read(readBuffer, bytesToRead);
+}
 
 void Eeprom::mSetReadWriteAddress(const uint16_t readWriteAddress)
 {
@@ -59,29 +66,6 @@ void Eeprom::mWaitUntilReady()
 
 /*
 
-// Writes data to the EEPROM.
-int eeprom_write(const uint8_t *source, size_t bytes_to_write)
-{
-    size_t rwaddr_offset = 2; // 2 address bytes
-    size_t total_length = bytes_to_write + rwaddr_offset;
-    uint8_t buffer[total_length];
-
-    for (int i = 0; i < total_length; i++)
-    {
-        if (i < rwaddr_offset)
-        {
-            buffer[i] = rwaddr[i];
-        }
-        else
-        {
-            buffer[i] = source[i - rwaddr_offset];
-        }
-    }
-
-    eeprom_wait_for_ready();
-    return i2c_write_blocking(eeprom_i2c, EEPROM_DEVICE_ADDRESS, buffer, total_length, false);
-}
-
 // Reads data from the EEPROM.
 int eeprom_read(uint8_t *dest, size_t bytes_to_read)
 {
@@ -96,20 +80,6 @@ void eeprom_clear_bytes(size_t bytes_to_clear)
     uint8_t clear_buffer[EEPROM_PAGE_MAX] = {0};
     eeprom_write(clear_buffer, bytes_to_clear);
     eeprom_wait_for_ready();
-}
-
-// Waits until EEPROM is ready for reading/writing.
-static int eeprom_wait_for_ready(void)
-{
-    uint8_t temp = 0;
-    int result = 0;
-
-    while ((result = i2c_read_timeout_us(eeprom_i2c, EEPROM_DEVICE_ADDRESS, &temp, 1, false, 10000)) == PICO_ERROR_GENERIC)
-    {
-        // Loop while EEPROM is unreadable or timeout is reached.
-        sleep_us(500); // Sleep to stop too many repeated requests.
-    }
-    return result;
 }
 
 */
