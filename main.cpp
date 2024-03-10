3f1e248a7acf77fd87187951985b5b46e73f1344
darkkii <anton_saarinen@hotmail.com>
2024-03-11 00:09:11 +0200

    moved mqtthandler as global to allow callback access to public member functions

--- main.cpp
+++ main.cpp
@@ -1,21 +1,26 @@
 #include <stdio.h>
+#include <memory>
+#include "MQTTClient.h"
 #include "MQTTHandler.h"
 
 using namespace std;
 
 #define USE_MQTT
 
+void messageHandler(MQTT::MessageData &md);
+// static global to allow callback access to public member functions
+static auto mqttHandler{ make_shared<MQTTHandler>(messageHandler) };
+
 int main()
 {
-    MQTTHandler mqttHandler;
-
     // Initialize chosen serial port
     stdio_init_all();
 
     printf("\nBoot\n");
 
+
 #ifdef USE_MQTT
-    mqttHandler.connect();
+    mqttHandler->connect();
     auto mqttTimeout = make_timeout_time_ms(5000);
 #endif
 
@@ -25,13 +30,24 @@ int main()
 #ifdef USE_MQTT
         if (time_reached(mqttTimeout))
         {
-            mqttHandler.update();
+            mqttHandler->update();
             mqttTimeout = make_timeout_time_ms(5000);
         }
 
-        mqttHandler.keepAlive();
+        mqttHandler->keepAlive();
 #endif
 
     }
 }
 
+void messageHandler(MQTT::MessageData &md)
+{
+    MQTT::Message &message = md.message;
+
+    mqttHandler->notifyObservers();
+
+    // printf("Message arrived: qos %d, retained %d, dup %d, packetid %d\n",
+    //        message.qos, message.retained, message.dup, message.id);
+    // printf("On topic %.*s\n", md.topicName.lenstring.len, md.topicName.lenstring.data);
+    // printf("Payload %.*s\n", (int)message.payloadlen, (char *)message.payload);
+}
